3bfbdd5bf0544a3acd3200851d1e99941172c6cd
Piotr Mińkowski <piotr_minkowski@wp.pl>
2019-02-17 15:15:24 +0100

    adding config-service

---
+++ config-service/src/main/kotlin/pl/piomin/services/config/ConfigApplication.kt
@@ -0,0 +1,8 @@
+package pl.piomin.services.config
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+class ConfigApplication
