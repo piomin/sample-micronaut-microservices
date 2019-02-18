f360877e1ec52452b9ce46590ce390dcb89c03af
Piotr Mińkowski <piotr_minkowski@wp.pl>
2019-02-19 00:22:32 +0100

    working jpa

--- department-service/src/main/kotlin/pl/piomin/services/department/model/Department.kt
+++ department-service/src/main/kotlin/pl/piomin/services/department/model/Department.kt
@@ -1,10 +1,10 @@
-package pl.piomin.services.department.model;
+package pl.piomin.services.department.model
 
-import javax.persistence.Entity
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
-data class Department(@Id var id: Long, var organizationId: Long, var name: String) {
+data class Department(@Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "department_id_seq") @SequenceGenerator(name = "department_id_seq", sequenceName = "department_id_seq") var id: Long,
+                      var organizationId: Long, var name: String) {
 
     @Transient
     var employees: MutableList<Employee> = mutableListOf()
--- department-service/src/main/kotlin/pl/piomin/services/department/repository/DepartmentRepository.kt
+++ department-service/src/main/kotlin/pl/piomin/services/department/repository/DepartmentRepository.kt
@@ -3,17 +3,16 @@ package pl.piomin.services.department.repository
 import io.micronaut.configuration.hibernate.jpa.scope.CurrentSession
 import io.micronaut.spring.tx.annotation.Transactional
 import pl.piomin.services.department.model.Department
-import javax.inject.Inject
 import javax.inject.Singleton
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
 @Singleton
-open class DepartmentRepository {
+open class DepartmentRepository(@param:CurrentSession @field:PersistenceContext val entityManager: EntityManager) {
 
-    @Inject
-    @CurrentSession
-    private lateinit var entityManager: EntityManager
+//    @Inject
+//    @CurrentSession
+//    private lateinit var entityManager: EntityManager
 
     @Transactional
     open fun add(department: Department): Department {
@@ -21,11 +20,14 @@ open class DepartmentRepository {
         return department
     }
 
-    fun findById(id: Long): Department = entityManager.find(Department::class.java, id)
+    @Transactional(readOnly = true)
+    open fun findById(id: Long): Department = entityManager.find(Department::class.java, id)
 
-    fun findAll(): List<Department> = entityManager.createQuery("SELECT d FROM Department d").resultList as List<Department>
+    @Transactional(readOnly = true)
+    open fun findAll(): List<Department> = entityManager.createQuery("SELECT d FROM Department d").resultList as List<Department>
 
-    fun findByOrganization(organizationId: Long) = entityManager.createQuery("SELECT d FROM Department d WHERE d.organizationId = :orgId")
+    @Transactional(readOnly = true)
+    open fun findByOrganization(organizationId: Long) = entityManager.createQuery("SELECT d FROM Department d WHERE d.organizationId = :orgId")
             .setParameter("orgId", organizationId)
             .resultList as List<Department>
 
