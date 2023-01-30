32bfdfeb61b9938eb17c745c40fdb3d2eea8b669
piomin <piotr.minkowski@gmail.com>
2023-01-30 16:11:09 +0100

    enable auto-update + add tests

---
+++ department-service/src/test/java/pl/piomin/services/department/DepartmentControllerTests.java
@@ -0,0 +1,50 @@
+package pl.piomin.services.department;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.instancio.Instancio;
+import org.instancio.Select;
+import org.junit.jupiter.api.Test;
+import pl.piomin.services.department.model.Department;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest
+public class DepartmentControllerTests {
+
+    @Inject
+    EmbeddedServer server;
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    void add() {
+        Department department = Instancio.of(Department.class)
+                .ignore(Select.field(Department::getId))
+                .create();
+        department = client.toBlocking()
+                .retrieve(HttpRequest.POST("/departments", department), Department.class);
+        assertNotNull(department);
+        assertNotNull(department.getId());
+    }
+
+    @Test
+    void findAll() {
+        Department[] departments = client.toBlocking().retrieve("/departments", Department[].class);
+        assertTrue(departments.length > 0);
+    }
+
+    @Test
+    void findById() {
+
+    }
+
+}
---
+++ employee-service/src/test/java/pl/piomin/services/employee/EmployeeControllerTests.java
@@ -0,0 +1,49 @@
+package pl.piomin.services.employee;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.instancio.Instancio;
+import org.instancio.Select;
+import org.junit.jupiter.api.Test;
+import pl.piomin.services.employee.model.Employee;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest
+public class EmployeeControllerTests {
+
+    @Inject
+    EmbeddedServer server;
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    void add() {
+        Employee employee = Instancio.of(Employee.class)
+                .ignore(Select.field(Employee::getId))
+                .create();
+        employee = client.toBlocking()
+                .retrieve(HttpRequest.POST("/employees", employee), Employee.class);
+        assertNotNull(employee);
+        assertNotNull(employee.getId());
+    }
+
+    @Test
+    void findAll() {
+        Employee[] employees = client.toBlocking().retrieve("/employees", Employee[].class);
+        assertTrue(employees.length > 0);
+    }
+
+    @Test
+    void findById() {
+
+    }
+}
